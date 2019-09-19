a1fd98f2c191adaa6715fca586a72d6565d39422
Ayush James <ayush.james1996@gmail.com>
2019-09-19 18:01:22 +1000

    social medias and mobile optimisation

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,15 +15,15 @@ console.log("It's Alive!");
 // });
 
 
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// window.onscroll = function() {myFunction()};
+//
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+//
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
