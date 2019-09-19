6c9ff48d751059ca045529b0fb5579594229028a
Ayush James <ayush.james1996@gmail.com>
2019-09-19 11:46:24 +1000

    something to display

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,29 @@
 console.log("It's Alive!");
 
-(window).scroll(function () {
-    console.log($(window).scrollTop());
-    var topDivHeight = $(".lander").height();
-    var viewPortSize = $(window).height();
+// (window).scroll(function () {
+//     console.log($(window).scrollTop());
+//     var topDivHeight = $(".lander").height();
+//     var viewPortSize = $(window).height();
+//
+//     var triggerAt = 10;
+//     var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+//
+//     if ($(window).scrollTop() >= triggerHeight) {
+//         $('.project_imgCards').css('visibility', 'visible').hide().fadeIn();
+//         $(this).off('scroll');
+//     }
+// });
 
-    var triggerAt = 10;
-    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
 
-    if ($(window).scrollTop() >= triggerHeight) {
-        $('.project_imgCards').css('visibility', 'visible').hide().fadeIn();
-        $(this).off('scroll');
-    }
-});
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
