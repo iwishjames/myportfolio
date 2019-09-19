04dec14c2ed6154125cde1d8b41f4c0a7e592bf9
Ayush James <ayush.james1996@gmail.com>
2019-09-19 23:51:03 +1000

    js click function active

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,29 @@
-console.log("Well, hello there! I have been expecting you 😉");
+console.log("It's Alive! MUAHAHAHAHA!!");
 
-function projectReveal() {
+function projectReveal0() {
+  if (document.getElementById("project0").classList.contains("projectReveal")) {
+    document.getElementById("project0").classList.remove("projectReveal");
+  } else
   document.getElementById("project0").classList.add("projectReveal");
+}
+
+function projectReveal1() {
+  if (document.getElementById("project1").classList.contains("projectReveal")) {
+    document.getElementById("project1").classList.remove("projectReveal");
+  } else
+  document.getElementById("project1").classList.add("projectReveal");
+}
+
+function projectReveal2() {
+  if (document.getElementById("project2").classList.contains("projectReveal")) {
+    document.getElementById("project2").classList.remove("projectReveal");
+  } else
+  document.getElementById("project2").classList.add("projectReveal");
+}
+
+function projectReveal3() {
+  if (document.getElementById("project3").classList.contains("projectReveal")) {
+    document.getElementById("project3").classList.remove("projectReveal");
+  } else
+  document.getElementById("project3").classList.add("projectReveal");
 }
