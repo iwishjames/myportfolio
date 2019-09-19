0c35d8a95393948a597846b841acb3e279fe9202
Ayush James <ayush.james1996@gmail.com>
2019-09-19 23:13:49 +1000

    mobile testing js test

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,29 +1,5 @@
-console.log("It's Alive!");
+console.log("Well, hello there! I have been expecting you 😉");
 
-// (window).scroll(function () {
-//     console.log($(window).scrollTop());
-//     var topDivHeight = $(".lander").height();
-//     var viewPortSize = $(window).height();
-//
-//     var triggerAt = 10;
-//     var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
-//
-//     if ($(window).scrollTop() >= triggerHeight) {
-//         $('.project_imgCards').css('visibility', 'visible').hide().fadeIn();
-//         $(this).off('scroll');
-//     }
-// });
-
-
-// window.onscroll = function() {myFunction()};
-//
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-//
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+function projectReveal() {
+  document.getElementById("project0").classList.add("projectReveal");
+}
